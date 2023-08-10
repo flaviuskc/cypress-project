c9ab7f14c3924ca8560a17016ef5caa04a24b502
Antonio Costa <akcosta@pdmfc.com>
2023-08-10 12:46:02 +0100

    test: reports with mochawesome

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,5 +2,17 @@ export default {
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4200",
-  },
+    setupNodeEvents(on, config){
+      "reporter"; "mochawesome"
+      "reporterOptions"; 
+        { 
+          "reportDir"; "cypress/report/mochawesome-report"
+          "overwrite"; true
+          "html"; true
+          "json"; false
+          "timestamp"; "mmddyyyy_HHMMss"
+          "charts"; true
+        }
+    }
+  }
 };
