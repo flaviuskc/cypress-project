6f471a0ba7fd70a51213e1d36f5e13597adaf83a
Antonio Costa <akcosta@pdmfc.com>
2023-08-10 16:47:39 +0100

    test: usage of intercept and request functions

--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -7,19 +7,38 @@ describe('login page', () => {
     beforeEach(() => {
         cy.visit('/')
     })
+
     it('should not be able to login', () => {
         cy.contains('user name is required!', { matchCase: false }).should('be.visible')
         cy.contains('password is required!', { matchCase: false }).should('be.visible')
     })
+
     it('should login correctly', () => {
-        cy.login(this.data.user, this.data.password)
-        cy.url().should('contain', `/user/${this.data.user}`)
+        cy.intercept('POST', 'http://localhost:3000/user/login', {
+            statusCode: 201
+        }).as('postLogin')
+        cy.login(Cypress.env('userName'), Cypress.env('password'))
+        cy.wait('@postLogin')
     })
+    
     it('should fill correctly the inputs but with no corresponding data on DB', () => {
         cy.intercept('POST', 'http://localhost:3000/user/login', {
             statusCode: 400
         }).as('stubPost')
         cy.login('antonio', '123now')
         cy.wait('@stubPost')
+    })
+
+    it('validate login API', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/user/login',
+            body: Cypress.env()
+        }).then(res => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('id')
+            expect(res.body.id).to.be.equal(3)
+        })
     })
 })
